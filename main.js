2b52fcb9a706be11a0f921bf657b874b3dbd5137
colorlessenergy <brianmunozdev@gmail.com>
2017-01-18 18:00:55 -0800

    first commit

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ var navBgColor = document.getElementsByClassName("nav")[0];
 var activeList = document.querySelectorAll("ul#onImage li");
 var gallery = document.querySelectorAll(".event");
 var counter = 2;
-
 dropDownButton.addEventListener("click", function () {
   dropDownMenu.classList.toggle("display-nav");
   dropDownMenu.classList.toggle("bg-color");
